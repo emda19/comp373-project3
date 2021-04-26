e8738d7f642f07a3d491d8919aa89317d9e49320
Kelvin Bui <47256422+kbui123@users.noreply.github.com>
2021-04-26 01:02:38 -0500

    Update Order.java

--- comp373-project3-patterns/src/model/order/Order.java
+++ comp373-project3-patterns/src/model/order/Order.java
@@ -1,5 +1,27 @@
 package model.order;
+import java.util.List;
+import model.clothing.Clothing;
 
-public class Order {
 
+public class Order implements IOrder {
+	private List<Clothing> purchasedClothing;
+	private float orderTotal;
+	private String orderStatus;
+	
+	public Order(List<Clothing> purchasedClothing, float orderTotal, String orderStatus) {
+		this.purchasedClothing = purchasedClothing;
+		this.orderStatus = orderStatus;	
+		this.orderTotal = orderTotal;
+			
+}
+	public List<Clothing> purchasedClothing() {
+		return this.purchasedClothing;
+	}
+	public float orderTotal() {
+		return this.orderTotal; 
+	}
+	public String orderStatus() {
+		return this.orderStatus;
+	}
+	
 }
